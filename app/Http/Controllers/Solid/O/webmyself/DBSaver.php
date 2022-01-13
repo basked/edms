2ebaf1be3391ff9f82f207b59e7842086301bab9
basked <basket.baza@gmail.com>
2022-01-13 23:26:18 +0300

    Add SOLID principle

--- app/Http/Controllers/Solid/O/webmyself/DBSaver.php
+++ app/Http/Controllers/Solid/O/webmyself/DBSaver.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Solid\O\webmyself;
 
 use App\Models\Report;
+use Illuminate\Database\Eloquent\Model;
 
 class DBSaver implements Saver
 {
+
+    protected Model $model;
+
+    /**
+     * @param Model $model
+     */
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
     public function save($data)
-   {
-       Report::create(['name'=>'report', 'data'=>json_encode($data),'is_active'=>true]);
+    {
+        $this->model->create(['name' => 'report', 'data' => json_encode($data), 'is_active' => true]);
     }
 
 }
